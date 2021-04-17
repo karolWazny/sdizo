d57d3d13759816e76d68d27ea34965a895c005b3
karolWazny <karoljanwazny@gmail.com>
2021-04-17 23:26:13 +0200

    porządki w Removerze czerwono czarnym

--- structure_templates_lib/trees_lib/rbutil/RBRemover.h
+++ structure_templates_lib/trees_lib/rbutil/RBRemover.h
@@ -131,23 +131,16 @@ void RBRemover<T>::remove(T key) {
                 doubleBlack = rbcast(successor->getRight());
             }
             //uwalniamy successora
-            auto successorParent = successor->getParent();
-            auto successorRight = successor->getRight();
-            successorParent->setLeft(successorRight);
-            successorRight->setParent(successorParent);
+            {
+                auto liberator = ConsequentLiberator<T>(root);
+                liberator.free(successor);
+            }
             //zastępujemy usuwany węzeł successorem
-            auto left = nodeToRemove->getLeft();
-            successor->setLeft(left);
-            left->setParent(successor);
-
-            auto right = nodeToRemove->getRight();
-            successor->setRight(right);
-            right->setParent(successor);
-
-            successor->setParent(parent);
-            parent->setSide(successor, nodeToRemoveSide);
-
-            root = RootFinder<T>(successor).find();
+            {
+                auto replacer = NodeReplacer<T>(nodeToRemove);
+                replacer.replaceWithNode(successor);
+                root = replacer.obtainRoot();
+            }
         }
         //jeżeli następnik był czarny lub usunęliśmy czarny liść
         if(thereIsDoubleBlack)
@@ -155,154 +148,6 @@ void RBRemover<T>::remove(T key) {
             resolveDoubleBlack(doubleBlack);
         }
     }
-
-    //stary kod
-    /*auto finder = KeyFinder<T>(root);
-    finder.setDesiredKey(key);
-    finder.find();
-    bool treeContainsThatKey = finder.nodeFound();
-    if(treeContainsThatKey)
-    {
-        auto nodeToRemove = rbcast(finder.getFound());
-        auto consequentFinder = ConsequentFinder<T>(nodeToRemove);
-        auto consequent = rbcast(consequentFinder.find());
-        auto consequentSide = consequentFinder.getConsequentSide();
-
-
-        if(consequent->isNil())
-        {
-            if(nodeToRemove == root)
-            {
-                root = RBFactory<T>::makeSentinel();
-                return;
-            }
-            auto customSentinel = RBFactory<T>().createSentinel(nodeToRemove);
-            nodeToRemove->setSide(customSentinel, consequentSide);
-            consequent = rbcast(customSentinel);
-        }
-
-        //v2
-        //zapisujemy w doubleParent pozycję w drzewie zajmowaną przez ojca następnika
-        auto doubleBlackParent = rbcast(consequent->getParent());
-        if(doubleBlackParent == nodeToRemove)
-            doubleBlackParent = consequent;
-        //zabezpieczenie przypadku granicznego
-
-        auto doubleBlackSide = Side::RIGHT;
-        if(doubleBlackParent->getRight() != consequent)
-            doubleBlackSide = Side::LEFT;
-
-        //zapamiętujemy kolor następnika
-        bool consequentWasBlack = consequent->isBlack();
-
-
-        auto liberator = ConsequentLiberator<T>(root);
-        liberator.free(consequent);
-        root = liberator.obtainRoot();
-        auto replacer = NodeReplacer<T>(nodeToRemove);
-        replacer.replaceWithNode(consequent);
-        if(root == nodeToRemove)
-            root = replacer.obtainRoot();
-
-        if(nodeToRemove->isRed() && consequent->isNil())
-            return;
-
-        //v2
-        //przekolorowanie następnika na kolor węzła usuwanego
-        if(nodeToRemove->isBlack())
-            consequent->paintBlack();
-        else
-            consequent->paintRed();
-        //v2
-        //
-        if(consequentWasBlack)
-        {
-            auto doubleBlack = rbcast(doubleBlackParent->get(doubleBlackSide));
-            if(doubleBlack->isNil())
-            {
-                doubleBlack = rbcast(RBFactory<T>().createSentinel(doubleBlackParent));
-                doubleBlackParent->setSide(doubleBlack, doubleBlackSide);
-            }
-
-            //kontrolujemy customową zaślepkę
-            int count = 0;
-
-            while(true)
-            {
-                if(doubleBlack->isRed() || (doubleBlackParent->isNil() && count > 0))
-                {
-                    doubleBlack->paintBlack();
-                    return;
-                }
-                count++;
-
-                //przygotowanie genealogii do przesuwania czarności w górę
-                auto doubleBlackSibling = rbcast(doubleBlackParent->get(!doubleBlackSide));
-
-                //przypadek: brat x jest czerwony (sprowadzenie do któregoś z kolejnych przypadków)
-                if(doubleBlackSibling->isRed())
-                {
-                    auto rotator = NodeRotator<T>();
-                    rotator.rotate(doubleBlackParent, doubleBlackSide);
-                    root = rotator.obtainRoot();
-                    doubleBlackParent->paintRed();
-                    doubleBlackSibling->paintBlack();
-                    doubleBlackSibling = rbcast(doubleBlackParent->get(!doubleBlackSide));
-                }
-                //przypadek: brat x jest czarny i ma czarnych synów
-                if(rbcast(doubleBlackSibling->getRight())->isBlack() && rbcast(doubleBlackSibling->getLeft())->isBlack())
-                {
-                    doubleBlackSibling->paintRed();
-                    doubleBlack = doubleBlackParent;
-
-                    //aktualizacja genealogii
-                    doubleBlackParent = rbcast(doubleBlack->getParent());
-                    doubleBlackSide = Side::RIGHT;
-                    if(doubleBlackParent->getRight() != doubleBlack)
-                        doubleBlackSide = Side::LEFT;
-                    doubleBlackSibling = rbcast(doubleBlackParent->get(!doubleBlackSide));
-
-                    continue;
-                }
-                //przypadek: brat x jest czarny i ma czerwonego syna z tej samej strony, czarnego z przeciwnej
-                if(rbcast(doubleBlackSibling->get(!doubleBlackSide))->isBlack())
-                {
-                    auto rotator = NodeRotator<T>();
-                    rotator.rotate(doubleBlackSibling, !doubleBlackSide);
-                    doubleBlackSibling->paintRed();
-                    rbcast(doubleBlackSibling->getParent())->paintBlack();
-
-                    //aktualizacja genealogii
-                    doubleBlackSibling = rbcast(doubleBlackParent->get(!doubleBlackSide));
-                }
-                //przypadek: brat x jest czarny i ma syna czerwonego syna z przeciwnej strony
-                if(rbcast(doubleBlackSibling->get(!doubleBlackSide))->isRed())
-                {
-                    auto rotator = NodeRotator<T>();
-                    rotator.rotate(doubleBlackParent, doubleBlackSide);
-                    root = rotator.obtainRoot();
-                    auto siblingWasBlack = doubleBlackSibling->isBlack();
-                    if(doubleBlackParent->isRed())
-                        doubleBlackSibling->paintRed();
-                    else
-                        doubleBlackSibling->paintBlack();
-
-                    if(siblingWasBlack)
-                        doubleBlackParent->paintBlack();
-                    else
-                        doubleBlackParent->paintRed();
-
-                    rbcast(doubleBlackSibling->get(!doubleBlackSide))->paintBlack();
-
-                    doubleBlackParent->paintBlack();
-                    //alternatywna próba:
-                    doubleBlack = doubleBlackParent;
-
-                    break;
-                }
-            }
-        }
-    }*/
 }
 
 template<typename T>
